2839d2fe995ad8159fe7f64e1b5d8c28ded032b5
Kachi <125273593+KachiBlockchain@users.noreply.github.com>
2023-11-22 15:29:15 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import React from 'react';
 import LogoWhite from '../assets/img/logo-white.svg'
 const Footer = () => {
   return <footer className="bg-primary py-12">
-    <div className='container mx-auto text-white flex justify-between'>
+    <div className='container mx-auto flex justify-between'>
       {/* Logo */}
       <img src={LogoWhite} alt="" />
-      <p className="bg-black">Copyright &copy; 2023. All rights reserved <b className='text-black'> KACHI </b>.</p>
+      Copyright &copy; 2023. All rights reserved <b className='text-accent'> KACHI </b>.
     </div>
   </footer>;
 };
